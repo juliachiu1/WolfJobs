0c7907e1d2f89558c1f5fe2a9d5244e439015c1e
Chloe-Ku <60029373+Chloe-Ku@users.noreply.github.com>
2022-09-26 14:05:28 -0400

    Update database.py

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_pymongo import pymongo
-from application import app
+
 CONNECTION_STRING = "mongodb+srv://wolfjobs:W00FW00F@cluster0.uj4oftq.mongodb.net/?retryWrites=true&w=majority"
 client = pymongo.MongoClient(CONNECTION_STRING)
 db = client.get_database('WolfJobs_DB')
