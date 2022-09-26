75febe69c9543f0fccdf7cc5c1f5e386c7cdd374
Chloe-Ku <60029373+Chloe-Ku@users.noreply.github.com>
2022-09-26 14:06:16 -0400

    Update apps.py

--- apps.py
+++ apps.py
@@ -1,15 +1,16 @@
 from flask import Flask
 from flask_pymongo import PyMongo
 from flask_mail import Mail
-
+import database
 
 class App:
     def __init__(self):
         self.app = Flask(__name__)
         self.app.secret_key = 'secret'
         self.app.config['MONGO_URI'] = 'mongodb://localhost:27017/test'
-        self.mongo = PyMongo(self.app)
-
+        #self.mongo = PyMongo(self.app)
+        self.mongo = database
+        
         self.app.config['MAIL_SERVER'] = 'smtp.gmail.com'
         self.app.config['MAIL_PORT'] = 465
         self.app.config['MAIL_USE_SSL'] = True
