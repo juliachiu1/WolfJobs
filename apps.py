7414b1bebe1ef2e4a8f8466d959c339249622017
Po-Hsun Lin <pohsunlin16@gmail.com>
2022-09-30 17:19:13 -0400

    add function 1

--- apps.py
+++ apps.py
@@ -10,10 +10,12 @@ class App:
         self.app.config['MONGO_URI'] = 'mongodb://localhost:27017/test'
         #self.mongo = PyMongo(self.app)
         self.mongo = database
-        
-        self.app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-        self.app.config['MAIL_PORT'] = 465
-        self.app.config['MAIL_USE_SSL'] = True
-        self.app.config['MAIL_USERNAME'] = "bogusdummy123@gmail.com"
-        self.app.config['MAIL_PASSWORD'] = "helloworld123!"
+
+        # self.app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+        # self.app.config['MAIL_PORT'] = 465
+        # self.app.config['MAIL_USE_SSL'] = True
+        # # self.app.config['MAIL_USERNAME'] = "wolfjobs.ncsu@gmail.com"
+        # # self.app.config['MAIL_PASSWORD'] = "W00FW00F"
+        # self.app.config['MAIL_USERNAME'] = "bogusdummy123@gmail.com"
+        # self.app.config['MAIL_PASSWORD'] = "helloworld123!"
         self.mail = Mail(self.app)
